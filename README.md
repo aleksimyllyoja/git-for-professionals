2084ed562a6b790ac4785c5cbd8d0b2f803416d4
Aleksi Myllyoja <aleksimyllyoja@Aleksis-MacBook-Pro.local>
2018-03-28 19:42:20 +0300

    asd

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # git-for-professionals
-A git tool for professionals
+
+# Listens every file change in a given directory, on every file change git adds everything, commits with a message "asd" and force pushes to the master.
+
+Work in progress.
---
+++ watch.py
@@ -0,0 +1,36 @@
+import sys
+import time
+import logging
+
+import git
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+path = sys.argv[1] if len(sys.argv) > 1 else '.'
+
+g = git.cmd.Git(path)
+
+class ProfessionalSystemEventHandler(FileSystemEventHandler):
+    def dispatch(self, event):
+
+        g.add("*")
+        g.commit(m="asd")
+        g.push(force=True)
+
+        super(ProfessionalSystemEventHandler, self).dispatch(event)
+
+
+event_handler = ProfessionalSystemEventHandler()
+
+observer = Observer()
+observer.schedule(event_handler, path, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
